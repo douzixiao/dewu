025551ab18dad0cfb93af58e5184acf92e62044e
mayuan <m18052054925@163.com>
2021-04-08 08:43:30 +0800

    first

---
+++ src/main/java/com/youkeda/dewu/control/Test2.java
@@ -0,0 +1,15 @@
+package com.youkeda.dewu.control;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Description:
+ * @Author: douzi
+ * @Date: 2021/4/8 8:40
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("hello "+now.toString());
+    }
+}
