0f0585b9b977da0277c43e644e71e171b3f22c06
douzixiao <3132172915@qq.com>
2021-04-08 08:50:52 +0800

    tijiao

---
+++ src/main/java/com/youkeda/dewu/control/Test2.java
@@ -0,0 +1,14 @@
+package com.youkeda.dewu.control;
+
+/**
+ * @program: dewu.app
+ * @description: dd
+ * @author: douzi
+ * @create: 2021-04-08 08:47
+ **/
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+}
